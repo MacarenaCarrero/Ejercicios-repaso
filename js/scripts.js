39e95a3d4980f6fb2d3953cc31aa9c450cd50055
MacarenaCarrero <macarena.c.rios@gmail.com>
2024-10-26 21:27:11 +0200

    Ejercicios-repaso

--- js/scripts.js
+++ js/scripts.js
@@ -64,15 +64,89 @@ console.log(resultMultiNumbers);
 
 //septimo
 
-const sumDigits =(number) =>{
-const num1= number [0]
-const num2 =number [1]
-const num3=number [2]
 
-const additionNumber = num1 +num2+num3
-return additionNumber
 
-} 
+//octavo
 
-const resultAdditionNumber =sumDigits ([326])
-console.log (resultAdditionNumber)
+const compareWords = (word1, word2) => {
+if (word1.length > word2.length){
+  return word1;
+  }else if (word1.length < word2.length){
+    return word2;
+  }else { 
+    return ("Las dos palabras son iguales")
+  }
+}
+
+const resultCompareWords = compareWords("avion", "flor")
+console.log(resultCompareWords)
+
+const resultCompareWords2 = compareWords("burro", "burro")
+console.log (resultCompareWords2)
+
+//noveno
+
+const getFirstElement = word => {
+  return word[0]
+}
+
+const resultGetFirstElement= getFirstElement ("Gatito")
+console.log (resultGetFirstElement)
+
+//decimo
+
+const getArrayElement = (word, position) =>{
+  return word[position]
+
+}
+const resultGetArrayElement =getArrayElement (["libro", "oso"],1);
+console.log(resultGetArrayElement)
+
+//undecimo
+
+const shortestWord =(word1, word2) => {
+  if (word1.length >word2.length){
+    return word2
+  } else if( word1.length < word2.length){
+    return word1
+  }else{
+    return "Ambas palabras tienen la misma longitud"
+  }
+}
+
+const resultShortestWord = shortestWord ("mariposa","agua")
+console.log (resultShortestWord)
+
+//duodecimo
+
+const onlyEven= (num1, num2, num3) => {
+  let numbers =[]
+
+  if(num1 % 2 ===0){
+    numbers.push(num1)
+  }
+  if (num2 % 2 ===0){
+    numbers.push(num2)
+  }
+  if( num3 % 2 ===0){
+    numbers.push(num3)
+  }
+  return numbers
+}
+
+const resultNumbers = onlyEven(31,20,40)
+console.log(resultNumbers)
+
+//decimotercero
+
+const baseExponent = number => {
+  const square =Math.pow (number,2)
+    const cube = Math.pow(number,3)
+    
+
+    return `El cuadrado es ${square} y el cubo es ${cube}`
+  
+}
+
+const resultBaseExponent = baseExponent(5)
+console.log (resultBaseExponent)
